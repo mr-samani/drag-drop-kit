908d9f4abf18ee1d863e439b7600f08db110fe07
mr.samani <mr.samani1368@gmail.com>
2026-06-27 10:38:51 +0330

    .

--- projects/ngx-drag-drop-kit/src/lib/ngx-drag-drop-kit.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-ngx-drag-drop-kit',
-  imports: [],
-  template: ` <p>ngx-drag-drop-kit works!</p> `,
-  styles: ``,
-})
-export class NgxDragDropKit {}
